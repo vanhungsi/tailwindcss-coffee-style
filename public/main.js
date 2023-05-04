1e61734f9972f8cad9702c3887ae4c6277b7d4d5
vanhungsi <127096791+vanhungsi@users.noreply.github.com>
2023-05-04 21:52:54 +0700

    header 04 may

--- public/main.js
+++ public/main.js
@@ -6,6 +6,7 @@ const toggleTopMenuIcon = document.getElementById('vhs-toggle-top-menu-icon')
 document.addEventListener('click',(e) => {
   if (toggleTopMenuIcon.contains(e.target)) {
     // Click to toggle top menu icon
+    topMenu.classList.toggle('vhs-topmenu-expanded')
     topMenu.classList.toggle('hidden')
   } else {
     // Click outside from toggle top menu icon
